3d7c131e3b972c328e42707a51f036ad442a300d
Parinda Darden <parinda.darden@gmail.com>
2013-12-14 20:45:20 -0500

    Add constraints to Comment model

--- db/migrate/20131215014055_create_comments.rb
+++ db/migrate/20131215014055_create_comments.rb
@@ -1,9 +1,9 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
-      t.text :comment
-      t.integer :blog_post_id
-      t.integer :author_id
+      t.text :comment, null: false
+      t.integer :blog_post_id, null:false
+      t.integer :author_id, null:false
 
       t.timestamps
     end
