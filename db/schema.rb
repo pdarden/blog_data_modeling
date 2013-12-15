db1917bc7e9e7f037ba582d865fdbd118a24e5b0
Parinda Darden <parinda.darden@gmail.com>
2013-12-14 20:47:04 -0500

    Create Category model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131215011817) do
+ActiveRecord::Schema.define(version: 20131215014055) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,23 @@ ActiveRecord::Schema.define(version: 20131215011817) do
   create_table "authors", force: true do |t|
     t.string   "name",       null: false
     t.string   "email",      null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "blog_posts", force: true do |t|
+    t.string   "title",       null: false
+    t.text     "body",        null: false
+    t.integer  "author_id",   null: false
+    t.integer  "category_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "comments", force: true do |t|
+    t.text     "comment",      null: false
+    t.integer  "blog_post_id", null: false
+    t.integer  "author_id",    null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
