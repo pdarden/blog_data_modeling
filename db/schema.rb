3a5e90e661cfd22854c980e5d1c42d3c86d768e3
Parinda Darden <parinda.darden@gmail.com>
2013-12-14 20:48:36 -0500

    Update schema with Category model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131215014055) do
+ActiveRecord::Schema.define(version: 20131215014646) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,12 @@ ActiveRecord::Schema.define(version: 20131215014055) do
     t.text     "body",        null: false
     t.integer  "author_id",   null: false
     t.integer  "category_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "categories", force: true do |t|
+    t.string   "name",       null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
