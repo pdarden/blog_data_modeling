a7f87a0cdad8fe42ae7d645def572ea9bde39508
Parinda Darden <parinda.darden@gmail.com>
2013-12-14 20:38:38 -0500

    Add constraints to Blog Post

--- db/migrate/20131215013645_create_blog_posts.rb
+++ db/migrate/20131215013645_create_blog_posts.rb
@@ -1,9 +1,9 @@
 class CreateBlogPosts < ActiveRecord::Migration
   def change
     create_table :blog_posts do |t|
-      t.string :title
-      t.string :body
-      t.integer :author_id
+      t.string :title, null: false
+      t.string :body, null: false
+      t.integer :author_id, null: false
       t.integer :category_id
 
       t.timestamps
