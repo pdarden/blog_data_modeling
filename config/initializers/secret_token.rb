368590f4c45dede2c676fdd8f3cdba90babd04b6
Parinda Darden <parinda.darden@gmail.com>
2013-12-14 20:15:59 -0500

    Initial commit, rails 4.0 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BlogDataModeling::Application.config.secret_key_base = 'ceee025a3a11bcc9f5fa825feae71618a0099d07efb1f807ff34d39dd37a6479263dbe0b80b3358cd8e5eef0b2e224efe48c6672f5f3c8bc5ff753235ace232f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BlogDataModeling::Application.config.session_store :cookie_store, key: '_blog_data_modeling_session'
